cd3a4f867cbe1e46e30845887b6deab54c008e2b
DoporteS1st3m4s <soporte.sistemas@o-lab.mx>
2024-03-06 15:49:42 -0600

    V.0.7

--- constants.js
+++ constants.js
@@ -1,10 +1,11 @@
-const DB_USER ="soportesistemas";
-const DB_PASSWORD = "dch77x9Kq2Di9R7n";
-const DB_HOST = "formulasnuevas.yythhzv.mongodb.net";
+const DB_USER ="arnaldo";
+const DB_PASSWORD = "StOl2019";
+const DB_HOST = "kaapadb-prod.mongocluster.cosmos.azure.com";
 const API_VERSION = "v1";
 const IP_SERVER="localhost";
 const JWT_SECRET_KEY = "GgLXeLCBfLr9CY9vYU2oOpjkWUR2D1pLHAniziVhKvZYNUud6VZC"
 
+const mongoDBString="mongodb+srv://<user>:<password>@kaapadb-prod.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000"
 module.exports={
     DB_USER,
     DB_PASSWORD,
