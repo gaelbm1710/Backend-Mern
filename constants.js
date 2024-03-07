25950bea5a751d4a9868d8956c09b66cfd02728d
DoporteS1st3m4s <soporte.sistemas@o-lab.mx>
2024-03-07 10:36:13 -0600

    V.0.8

--- constants.js
+++ constants.js
@@ -1,16 +1,27 @@
+//Conexion a COSMOS
 const DB_USER ="arnaldo";
 const DB_PASSWORD = "StOl2019";
 const DB_HOST = "kaapadb-prod.mongocluster.cosmos.azure.com";
 const API_VERSION = "v1";
 const IP_SERVER="localhost";
 const JWT_SECRET_KEY = "GgLXeLCBfLr9CY9vYU2oOpjkWUR2D1pLHAniziVhKvZYNUud6VZC"
-
 const mongoDBString="mongodb+srv://<user>:<password>@kaapadb-prod.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000"
+
+//conexion a Postgresql
+const PG_CONEXION = {
+    PG_USER:"carlo_banuelos@omicrondxpprod",
+    PG_PASSWORD: "QK5aV315J5",
+    PG_HOST: "omicrondxpprod.postgres.database.azure.com",
+    PG_DATABASE: "omicron_dxp",
+}
+
+
 module.exports={
     DB_USER,
     DB_PASSWORD,
     DB_HOST,
     API_VERSION,
     IP_SERVER,
-    JWT_SECRET_KEY
+    JWT_SECRET_KEY,
+    PG_CONEXION
 };
--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
 const app = require("./app");
+const {Pool} =  require("pg")
 const{
     DB_USER,
     DB_PASSWORD,
     DB_HOST,
     API_VERSION,
-    IP_SERVER 
+    IP_SERVER,
+    PG_CONEXION 
 } = require("./constants");
 
 const PORT = process.env.POST || 3977
@@ -14,7 +16,7 @@ const connectDB = async () => {
   try {
     await mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/`)
 
-    console.log('La conexión con la base de datos ha sido exitosa.');
+    console.log('La conexión con la base de datos MONGODB ha sido exitosa.');
     app.listen(PORT, () => {
       console.log('API REST')
       console.log(`http://${IP_SERVER}:${PORT}/api/${API_VERSION}/`)
@@ -24,7 +26,18 @@ const connectDB = async () => {
   }
 }
 
+
+const connectPG = async () =>{
+  try {
+    const pool = new Pool(PG_CONEXION)
+    console.log('La conexión con la base de datos POSTGRESQL ha sido exitosa.');
+  } catch (error) {
+    console.log("Error al conectarse a la base de datos de PostgreSQL ", error);
+  }
+}
+
 connectDB();
+connectPG();
 
 app.get("/",(req, res) =>{
   res.send("Si jala")
