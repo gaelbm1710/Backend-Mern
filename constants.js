1c7388056d51b2375ec40d074394c89e27ac0220
DoporteS1st3m4s <soporte.sistemas@o-lab.mx>
2024-03-07 12:08:42 -0600

    Pruebas

--- constants.js
+++ constants.js
@@ -1,3 +1,13 @@
+//Conexion a MONGODB
+const DB_USER ="soportesistemas";
+const DB_PASSWORD = "dch77x9Kq2Di9R7n";
+const DB_HOST = "formulasnuevas.yythhzv.mongodb.net";
+const API_VERSION = "v1";
+const IP_SERVER="localhost";
+const JWT_SECRET_KEY = "GgLXeLCBfLr9CY9vYU2oOpjkWUR2D1pLHAniziVhKvZYNUud6VZC"
+const mongoDBString="mongodb+srv://<user>:<password>@kaapadb-prod.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000"
+
+/*
 //Conexion a COSMOS
 const DB_USER ="arnaldo";
 const DB_PASSWORD = "StOl2019";
@@ -6,7 +16,7 @@ const API_VERSION = "v1";
 const IP_SERVER="localhost";
 const JWT_SECRET_KEY = "GgLXeLCBfLr9CY9vYU2oOpjkWUR2D1pLHAniziVhKvZYNUud6VZC"
 const mongoDBString="mongodb+srv://<user>:<password>@kaapadb-prod.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000"
-
+*/
 //conexion a Postgresql
 const PG_CONEXION = {
     PG_USER:"carlo_banuelos@omicrondxpprod",
