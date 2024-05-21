82a4f2b25730ec7eea4cc8e1d23858fb974bde7b
SoporteS1st3m4s <soporte.sistemas@o-lab.mx>
2024-05-21 12:27:39 -0600

    Actualizacion  21/05/2024 12:27

--- Controllers/contabilidad.js
+++ Controllers/contabilidad.js
@@ -35,6 +35,7 @@ async function getReporteK(req,res){
     }
 }
 
+
 async function getReporteTranscredito(req, res){
     try {
         const page = req.query.page || 1;
@@ -107,10 +108,85 @@ async function getPagosFacturas(req, res){
     }
 }
 
+//Reporte Filtrado por Fechas
+async function getReporteKeylaFechas(req, res){
+    try {
+        const page = req.query.page || 1;
+        const limit = req.query.limit || 10;
+        const fecha1 = req.query.fecha1;
+        const fecha2 = req.query.fecha2; 
+        const offset = (page - 1)*limit;
+        const values = [limit, offset, fecha1, fecha2];
+        const query = `SELECT * FROM vw_reportekeyla WHERE createdate BETWEEN $3 AND $4 LIMIT $1 OFFSET $2`;
+        const reportekeyla = await pool.query(query, values);
+        res.json(reportekeyla.rows)
+    } catch (error) {
+        console.log(error);
+        res.status(400).send({msg: "Error al obtener la información", error})
+    }
+}
+
+async function getReporteTranscreditoFechas(req, res){
+    try {
+        const page = req.query.page || 1;
+        const limit = req.query.limit || 10;
+        const fecha1 = req.query.fecha1;
+        const fecha2 = req.query.fecha2; 
+        const offset = (page - 1)*limit;
+        const values = [limit, offset, fecha1, fecha2];
+        const query = ` SELECT * FROM vw_reportecredito WHERE fechacreacion BETWEEN $3 AND $4 LIMIT $1 OFFSET $2`
+        const reportekeyla = await pool.query(query, values);
+        res.json(reportekeyla.rows)
+    } catch (error) {
+        console.log(error);
+        res.status(400).send({msg: "Error al obtener la información", error})
+    }
+}
+
+async function getPagosFacturasFechas(req, res){
+    try {
+        const page = req.query.page || 1;
+        const limit = req.query.limit || 10;
+        const fecha1 = req.query.fecha1;
+        const fecha2 = req.query.fecha2; 
+        const offset = (page - 1)*limit;
+        const values = [limit, offset, fecha1, fecha2];
+        const query = `select u.cardcode, u.nombre, to_char(i.createdate, 'YYYY-MM-DD') as Fecha_Creacion, to_char(i.paydate, 'YYYY-MM-DD') as Fecha_Pago, 
+        i.status, i2.invoiceid Factura, i.totalcost Total_Factura, u.correo, 
+        case 
+            when paymentmethod -> 'PaymentMethod' ->> 'Object' = 'bbvamulti' and paymentmethod -> 'PaymentMethod' ->> 'Type' = 'TDX' then 'BBVA TARJETA CREDITO/DEBITO'
+            when paymentmethod -> 'PaymentMethod' ->> 'Object' = 'bbvamulti' and paymentmethod -> 'PaymentMethod' ->> 'Type' = 'CIE' then 'BBVA CIE INTERBANCARIO'
+            when paymentmethod -> 'PaymentMethod' ->> 'Object' = 'bbvamulti' and paymentmethod -> 'PaymentMethod' ->> 'Type' = 'CIE_INTER' then 'BBVA SPEI BBVA'
+            when paymentmethod -> 'PaymentMethod' ->> 'Object' = 'bbvamulti' and paymentmethod -> 'PaymentMethod' ->> 'Type' = 'SUC' then 'BBVA PRACTICAJA/SUCURSAL'
+            when paymentmethod -> 'PaymentMethod' ->> 'Brand' = '' and paymentmethod  -> 'PaymentMethod' ->> 'Object' = 'credit_payment' then 'Crédito'
+            when paymentmethod -> 'PaymentMethod' ->> 'Object' = 'card_payment' and paymentmethod -> 'PaymentMethod' ->> 'Type' = 'debit' then 'Tarjeta Débito'
+            when paymentmethod -> 'PaymentMethod' ->> 'Object' = 'card_payment' and paymentmethod -> 'PaymentMethod' ->> 'Type' = 'credit' then 'Tarjeta Crédito'
+            when paymentmethod -> 'PaymentMethod' ->> 'Object' = 'cash_payment' then 'Oxxo'
+            when paymentmethod -> 'PaymentMethod' ->> 'Object' = 'bank_transfer_payment' then 'SPEI'
+            when paymentmethod -> 'PaymentMethod' ->> 'Object' = 'paypal' then 'Paypal'
+            else 'Revisar en SAP'
+        end as Metodo_pago
+        from invoicepayments i left join userslistinsert u on i.cardcode = u.cardcode left join invoicepaymentdetail i2 on i.transactionid = i2.transactionid
+        WHERE i.createdate BETWEEN $3 AND $4
+        order by i.createdate desc 
+        LIMIT $1 OFFSET $2`
+        const reportekeyla = await pool.query(query, values);
+        res.json(reportekeyla.rows)
+    } catch (error) {
+        console.log(error);
+        res.status(400).send({msg: "Error al obtener la información", error})
+    }
+}
+
+
+
 module.exports={
     getReporteKeyla,
+    getReporteKeylaFechas,
     getReporteK,
     getReporteTranscredito,
+    getReporteTranscreditoFechas,
     getClientesCredito,
-    getPagosFacturas
+    getPagosFacturas,
+    getPagosFacturasFechas
 }
--- router/contabilidad.js
+++ router/contabilidad.js
@@ -8,6 +8,10 @@ api.get("/contas",ContaController.getReporteK);
 api.get("/credito",ContaController.getReporteTranscredito);
 api.get("/creditos",ContaController.getClientesCredito);
 api.get("/pagos",ContaController.getPagosFacturas);
+//Filtros de Fechas
+api.get("/contar",ContaController.getReporteKeylaFechas);
+api.get("/creditor",ContaController.getReporteTranscreditoFechas);
+api.get("/pagar",ContaController.getPagosFacturasFechas)
 
 
 module.exports=api;
